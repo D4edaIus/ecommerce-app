12d76ba22e898c2bbb0cbd85bbe4ad0dee4d70bd
Dang Duy <dangngocduy0510@outlook.com>
2023-04-03 14:24:20 +0700

    1st commit

---
+++ WebApplication1/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_movies>().HasKey(am => new
+            {
+                am.IDActor,
+                am.IdMovies
+            });
+            modelBuilder.Entity<Actor_movies>().HasOne(m => m.movies).WithMany(am => am.Actor_Movies).HasForeignKey(m => m.IdMovies);
+            modelBuilder.Entity<Actor_movies>().HasOne(m => m.actor).WithMany(am => am.Actor_Movies).HasForeignKey(m => m.IDActor);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Actor> actors { get; set; }
+        public DbSet<Movies> movies { get; set; }
+        public DbSet<Actor_movies> actors_movies { get; set; }
+        public DbSet<Cinema> Cinema { get; set; }
+        public DbSet<Producer> producers { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Movies.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Data;
+
+namespace WebApplication1.Models
+{
+    public class Movies
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MoviesCategories MoviesCategory { get; set; }
+        public List<Actor_movies> Actor_Movies { get; set; }
+        public int IDCinema { get; set; }
+        [ForeignKey("IDCinema")]
+        public Cinema cinema { get; set; }
+        public int IDProducer { get; set; }
+        [ForeignKey("IDProducer")]
+        public Producer producer { get; set; }
+    }
+}
